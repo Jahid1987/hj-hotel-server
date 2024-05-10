35aecf1b234bb714d4523ff7b5132a5d4018f703
Jahid Anwer <jahid.englaa@gmail.com>
2024-05-10 12:05:34 +0600

    database and token related settings done

--- index.js
+++ index.js
@@ -1,9 +1,97 @@
 const express = require("express");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
 const app = express();
+require("dotenv").config();
+
 const port = process.env.PORT || 5000;
 
+// BrNcamoBXneJrEZA
+// carDoctor
+
+// middleware package
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "http://localhost:5174"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+// custom middleware (token veryfying)
+async function veryfyToken(req, res, next) {
+  const token = req.cookies?.token;
+  // console.log(token);
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized" });
+  } else {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRETE, (err, decoded) => {
+      if (err) {
+        // console.log(err);
+        return res.status(401).send({ message: "unauthorized" });
+      } else {
+        // console.log("value in the token", decoded);
+        req.user = decoded;
+        next();
+      }
+    });
+  }
+}
+
+// mondoDB
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6iad9fh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // app cookes related api
+    // cookie option
+    const cookieOptions = {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+    };
+    // setting cookies from login page from the client side
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRETE, {
+        expiresIn: "1h",
+      });
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
+
+    // deleting token
+    app.post("/logout", async (req, res) => {
+      const user = req.body;
+      console.log("user from logout: ", user);
+      res
+        .clearCookie("token", { ...cookieOptions, maxAge: 0 })
+        .send({ success: true });
+    });
+    // database related api will be here
+    // Get the database and collection on which to run the operation
+    const database = client.db("carDoctorDB");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Server is running");
 });
 
 app.listen(port, () => {
